3c1c8b956d3103f6238fbdb9aa08cd928732a49c
samuel <samuel@Lima.spamplex.net>
2019-03-21 03:50:20 -0400

    Initial commit

---
+++ rules.mk
@@ -0,0 +1,5 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+RGBLIGHT_ENABLE = no
+  
+AUTO_SHIFT_ENABLE = yes
